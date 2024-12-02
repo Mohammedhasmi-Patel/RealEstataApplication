29717e3fdac64c5482788ec36929d495d94a31c5
Mohammedhasmi-Patel <patelmohammedhasmi@gmail.com>
2024-12-02 17:08:56 +0530

    database connection successfull

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import { DbCon } from "./utils/db.js";
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT;
+
+// mognodb connection
+DbCon();
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);

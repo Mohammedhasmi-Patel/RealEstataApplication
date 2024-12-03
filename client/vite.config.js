007992c12994b6cbebd516628ccc032d7df6893b
Mohammedhasmi-Patel <patelmohammedhasmi@gmail.com>
2024-12-03 13:01:03 +0530

    complete signup functioanlity

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080", // Corrected the target definition
+        changeOrigin: true, // Adds support for changing the origin of the host header
+        secure: false, // Skips SSL verification for development
+      },
+    },
+  },
   plugins: [react()],
-})
+});
